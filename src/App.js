4f857dc4202763225f16afa502f67cfcf0adea31
seohyon-cho <flanique@naver.com>
2023-12-26 12:45:05 +0900

    Router 기본 셋팅

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Route } from 'react-router-dom';
+import Menu from './Menu';
+import Main from './Main';
+import UserName from './UserName';
+import UserAddress from './UserAddress';
 
 function App() {
 	const queryClient = new QueryClient();
 
 	return (
 		<QueryClientProvider client={queryClient}>
-			<h1>Hello</h1>
+			<Menu />
+			<Route exact path='/' component={Main} />
+			<Route path='/name' component={UserName} />
+			<Route path='/address' component={UserAddress} />
 			<ReactQueryDevtools />
 		</QueryClientProvider>
 	);

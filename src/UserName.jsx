a305d2a1270cf6718b0d7c575bc287560a1730c5
seohyon-cho <flanique@naver.com>
2023-12-27 09:58:37 +0900

    staleTime, cacheTime을 관리하는 이유 및 차이점

--- src/UserName.jsx
+++ src/UserName.jsx
@@ -1,12 +1,18 @@
+import { useState } from 'react';
 import { useUserQuery } from './hooks/useUserQuery';
 
 export default function UserName() {
-	const result = useUserQuery(1);
-	console.log(result);
+	const [Num, setNum] = useState(1);
+	// 커스텀 훅은 핸들러 함수나 useEffect 안에 넣을 수 없음.
+	const { isSuccess, data } = useUserQuery(Num);
 
 	return (
 		<div className='UserName'>
 			<h1>UserName</h1>
+			<button onClick={() => setNum(1)}>변경1</button>
+			<button onClick={() => setNum(2)}>변경2</button>
+
+			<h2>{isSuccess && data.name}</h2>
 		</div>
 	);
 }
--- src/hooks/useUserQuery.js
+++ src/hooks/useUserQuery.js
@@ -2,14 +2,21 @@ import { useQuery } from '@tanstack/react-query';
 
 const fetchUser = async ({ queryKey }) => {
 	console.log(queryKey);
-	const response = await fetch('https://jsonplaceholder.typicode.com/users');
+	const response = await fetch(`https://jsonplaceholder.typicode.com/users/${queryKey[1]}`);
 	return await response.json();
 };
 
 export const useUserQuery = (num) => {
 	// useQuery에 첫 번째 인수로 넣는 값은 무조건 fetching함수(여기서는 fetchUser)로 전달됨.
 	// useQuery에서는 queryKey가 동일하면 같은 데이터라고 인지하기 때문에, re-fetching 처리를 하지 않음.
-	return useQuery(['users', num], fetchUser, { refetchOnMount: false, refetchOnWindowFocus: false, cacheTime: 1000 * 5, staleTime: 1000 * 0 });
+
+	// cacheTime : inactive가 된 상태에서 얼마동안 데이터를 캐시에 유지시킬지에 대한 시간 정보
+	// staleTime : 서버 데이터를 fetching한 순간부터 언제까지 최신 데이터로 인지 시키면서 cacheTime을 소진시키지 않을지에 대한 시간 값
+
+	// [ 좀 더 이해하기 쉬운 버전 설명 ]
+	// cacheTime : 해당 데이터를 컴포넌트에서 활용하지 않더라도 (= inactive 상태), 얼마동안 gc(garbage collector)에 의한 데이터 삭제를 막을지에 대한 시간 값 설정
+	// staleTime : 얼마동안 re-fetching을 하지 않을지 (= 최신 데이터 취급할지) 에 대한 시간 값 설정
+	return useQuery(['users', num], fetchUser, { refetchOnMount: false, refetchOnWindowFocus: false, cacheTime: 1000 * 20, staleTime: 1000 * 5 });
 };
 
 /*

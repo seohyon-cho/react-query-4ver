6a72b2b95b8777109b84e9244e75592c7755ce13
seohyon-cho <flanique@naver.com>
2023-12-27 11:49:56 +0900

    mutate를 이용해서 기존 서버데이터 변경

--- src/UserName.jsx
+++ src/UserName.jsx
@@ -1,11 +1,24 @@
-import { useUserQuery } from './hooks/useUserQuery';
+import { useState } from 'react';
+import { useUpdateUser, useUserQuery } from './hooks/useUserQuery';
 
 export default function UserName() {
 	const { isSuccess, data } = useUserQuery();
+	const updateUser = useUpdateUser();
+	const [UserName, setUserName] = useState('');
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		updateUser.mutate([UserName, 2]);
+	};
+
+	console.log(data);
 
 	return (
 		<div className='UserName'>
 			<h1>User Name</h1>
+
+			<form onSubmit={handleSubmit}>
+				<input type='text' value={UserName || ''} onChange={(e) => setUserName(e.target.value)} />
+			</form>
 
 			<ul>{isSuccess ? data.map((user) => <li key={user.id}>{user.name}</li>) : <p>Loading...</p>}</ul>
 		</div>
--- src/hooks/useUserQuery.js
+++ src/hooks/useUserQuery.js
@@ -6,8 +6,8 @@ const fetchUser = async () => {
 };
 
 // 기존
-const deleteUser = async ({ queryKey }) => {
-	const response = await fetch(`https://jsonplaceholder.typicode.com/users/${queryKey[1]}`, {
+const deleteUser = async (num) => {
+	const response = await fetch(`https://jsonplaceholder.typicode.com/users/${num}`, {
 		method: 'DELETE',
 	});
 	return await response.json();
@@ -18,16 +18,26 @@ export const useUserQuery = () => {
 	return useQuery(['users'], fetchUser, { refetchOnMount: false, refetchOnWindowFocus: false, cacheTime: 1000 * 20, staleTime: 1000 * 0 });
 };
 
-// 인수로 순번을 받아서, 해당 순번의 데이터를 삭제하는 커스텀 훅
-export const useDeleteQuery = () => {
-	// 기존에 생성한 queryClient 인스턴스를 호출.
-	// 해당 queryClient 인스턴스에서 활용할 수 있는 prototype method인 setQueryData 라는 서버 데이터 변경 요청 값을 등록하는 함수를 가져올 수 있음.
+// 기존 서버 데이터 업데이트 함수
+export const updateUser = async ([userName, num]) => {
+	const response = await fetch(`https://jsonplaceholder.typicode.com/users/${num}`, {
+		method: 'PATCH',
+		body: JSON.stringify({
+			name: userName,
+		}),
+		headers: {
+			'Content-type': 'application/json; charset=UTF-8',
+		},
+	});
+	const result = await response.json();
+	console.log(result);
+	return result;
+};
+
+// 데이터 변경 커스텀 훅
+export const useUpdateUser = () => {
 	const queryClient = useQueryClient();
-	// useMutation은, 첫 번째 인수로는 비동기 데이터 변경 함수, 두 번째 인수로는 옵션 설정 객체가 들어가야 함.
-	// useMutation(비동기데이터 변경함수, 옵션설정객체 {onSuccess: mutate요청이 성공적으로 수행되면 연결될 핸들러 함수})
-	// useMutation 훅이 deleteUser라는 내부 fetching 함수를 호출해서 서버 데이터를 변경 요청.
-	return useMutation(deleteUser, {
-		// 서버 데이터 변경 성공 시, 변경된 서버 데이터 값을 다시 고유의 queryKey로 등록해서 react-query가 비동기 데이터를 관리하게 함.
+	return useMutation(updateUser, {
 		onSuccess: (args) => {
 			queryClient.setQueryData(['users', args.id], args);
 		},
